e5b503fa093e6a0ef3afc7419d01361f70a860c3
Wes <wesleyayer92@gmail.com>
2020-06-08 16:04:24 -0400

    adds 16 cards for game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './MemoryCard.css';
+import MemoryCard from './components/MemoryCard';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Memory Game
+          <p style={{fontSize: "15px", color: "#767676"}}>Match Cards To Win</p>
       </header>
+      <div>
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
+      <div>
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
+      <div>
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
+      <div>
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
     </div>
   );
 }
---
+++ src/components/MemoryCard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../MemoryCard.css';
+
+export default class MemoryCard extends Component {
+    render() {
+        return (
+            <div className="MemoryCard">
+                <img src="https://www.digitalcrafts.com/img/logo-wrench-white.png" alt="" />
+            </div>
+        );
+    }
+}
